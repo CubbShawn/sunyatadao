8936c1ac7d1a771326f53b1c4b397c27c4935c4e
Shawn Cubbedge <90860023+CubbShawn@users.noreply.github.com>
2021-12-19 21:06:50 +0000

    Added wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useMemo, useState } from "react";
+
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to SunyataDao</h1>
+        <button onClick={() => connectWallet("injected")} className="button">
+          Connect your wallet
+        </button>
+      </div>
+    );
+
+  }
+  
+ 
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
 };
+const mySecret = process.env['w.info']
 
 export default App;
